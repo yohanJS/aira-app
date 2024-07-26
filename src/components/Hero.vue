3a492929d28d7a31fe0374056494a516ddc3fbf6
Yohan Garcia <yoanvaldes03@gmail.com>
2024-07-26 01:26:40 -0400

    first commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container-fluid p-0 bg-color">
+    <div class="row g-0">
+      <div class="col-md-12 shadow-lg position-relative">
+        <div class="row d-flex flex-column pt-5 align-items-center text-center text-white" style="position: absolute; top: 0; left: 0; width: 100%; height: 100vh; z-index: 1;">
+          <div id="section" class="col-12 mt-5">
+       
+          </div>
+          <div class="col-12">
+            <h1 class="display-1">Meet Aeero, Your Intelligent Companion</h1>
+          </div>
+          <div class="row">
+            <div class="col-12 mt-5">
+              <p id="welcomeMsg" class="mt-4 pb-3">
+              Experience the future of AI assistance with our cutting-edge technology.
+              </p>    
+            </div>
+          </div>
+         </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.bg-color {
+  background-color: #0a323d;
+}
+.btn-color {
+  background-color: #CFDD51;
+}
+.btn-text-color {
+  color: #1C434D;
+}
+.glass-button {
+  background-color: rgba(255, 255, 255, 0.1); /* Adjust the opacity (0.3) as needed */
+  backdrop-filter: blur(10px); /* Adjust the blur value as needed */
+}
+.btn-border-color {
+  border: 1px solid #CFDD51;
+}
+.text-color {
+  color: #cfdd51;
+}
+.mr-r {
+  margin-right: 20px;
+}
+</style>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const isMobile = ref(false);
+const isDesktop = ref(false);
+
+onMounted(() => {
+isMobile.value = window.innerWidth < 600;
+});
+
+window.addEventListener('resize', () => {
+isMobile.value = window.innerWidth < 600;
+let welcomeMsg = document.getElementById("welcomeMsg");
+
+if (isMobile.value) {
+  welcomeMsg.classList.remove("mt-4");
+} else {
+  welcomeMsg.classList.add("mt-4");
+  freeQuoteBtn.classList.add("mr-r");
+}
+});
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,240 @@
+<template>
+  <!--MOBILE VERSION-->
+  <nav id="home" class="navbar p-0 glass-btn" v-if="isMobile">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        Aeero
+      </a>
+      <button class="navbar-toggler p-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+        aria-controls="offcanvasNavbar">
+        <span>
+          <div class="toggler-icon">
+            <svg width="20" height="20" viewBox="0 0 42 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0 28V23.3333H42V28H0ZM0 16.3333V11.6667H42V16.3333H0ZM0 4.66667V0H42V4.66667H0Z"
+                fill="#36454F" />
+            </svg>
+          </div>
+        </span>
+      </button>
+      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div class="offcanvas-header secondary-bg-color p-2">
+          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
+            Aeero
+          </h5>
+          <a href="#" class="nav-link m-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tiktok"
+              viewBox="0 0 16 16">
+              <path
+                d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
+            </svg>
+          </a>
+          <a href="#" class="nav-link m-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook"
+              viewBox="0 0 16 16">
+              <path
+                d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
+            </svg>
+          </a>
+          <a href="#" class="nav-link m-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram"
+              viewBox="0 0 16 16">
+              <path
+                d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
+            </svg>
+          </a>
+          <a href="#" class="nav-link m-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pinterest"
+              viewBox="0 0 16 16">
+              <path
+                d="M8 0a8 8 0 0 0-2.915 15.452c-.07-.633-.134-1.606.027-2.297.146-.625.938-3.977.938-3.977s-.239-.479-.239-1.187c0-1.113.645-1.943 1.448-1.943.682 0 1.012.512 1.012 1.127 0 .686-.437 1.712-.663 2.663-.188.796.4 1.446 1.185 1.446 1.422 0 2.515-1.5 2.515-3.664 0-1.915-1.377-3.254-3.342-3.254-2.276 0-3.612 1.707-3.612 3.471 0 .688.265 1.425.595 1.826a.24.24 0 0 1 .056.23c-.061.252-.196.796-.222.907-.035.146-.116.177-.268.107-1-.465-1.624-1.926-1.624-3.1 0-2.523 1.834-4.84 5.286-4.84 2.775 0 4.932 1.977 4.932 4.62 0 2.757-1.739 4.976-4.151 4.976-.811 0-1.573-.421-1.834-.919l-.498 1.902c-.181.695-.669 1.566-.995 2.097A8 8 0 1 0 8 0" />
+            </svg>
+          </a>
+          <div class="col-xs-12">
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+        </div>
+        <div class="offcanvas-body bg-color">
+          <button class="btn rounded-pill btn-grad text-dark fw-bold m-0"  onclick="window.location.href='#generateReview'">Write Review</button>
+          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+              <a class="nav-link" href="#">Link</a>
+              <a class="nav-link" href="#">Link</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <nav id="home" class="navbar-custom bg-color" v-else>
+    <div class="container-fluid">
+      <div class="row text-dark">
+        <div class="col-4 d-flex justify-content-center align-items-center">
+          <a class="nav-link m-3" href="#">Home</a>
+          <a class="nav-link m-3" href="#">Link</a>
+          <a class="nav-link m-3" href="#">Link</a>
+          <a class="nav-link m-3" href="#">Link</a>
+        </div>
+        <div class="col-3 d-flex justify-content-center align-items-center">
+          <a class="display-6 text-dark" href="#">
+            Aeero
+          </a>
+        </div>
+        <div class="col-5 d-flex justify-content-center align-items-center">
+          <a href="#" class="nav-link m-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tiktok"
+              viewBox="0 0 16 16">
+              <path
+                d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
+            </svg>
+          </a>
+          <a href="#" class="nav-link m-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook"
+              viewBox="0 0 16 16">
+              <path
+                d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
+            </svg>
+          </a>
+          <a href="#" class="nav-link m-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram"
+              viewBox="0 0 16 16">
+              <path
+                d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
+            </svg>
+          </a>
+          <a href="#" class="nav-link m-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pinterest"
+              viewBox="0 0 16 16">
+              <path
+                d="M8 0a8 8 0 0 0-2.915 15.452c-.07-.633-.134-1.606.027-2.297.146-.625.938-3.977.938-3.977s-.239-.479-.239-1.187c0-1.113.645-1.943 1.448-1.943.682 0 1.012.512 1.012 1.127 0 .686-.437 1.712-.663 2.663-.188.796.4 1.446 1.185 1.446 1.422 0 2.515-1.5 2.515-3.664 0-1.915-1.377-3.254-3.342-3.254-2.276 0-3.612 1.707-3.612 3.471 0 .688.265 1.425.595 1.826a.24.24 0 0 1 .056.23c-.061.252-.196.796-.222.907-.035.146-.116.177-.268.107-1-.465-1.624-1.926-1.624-3.1 0-2.523 1.834-4.84 5.286-4.84 2.775 0 4.932 1.977 4.932 4.62 0 2.757-1.739 4.976-4.151 4.976-.811 0-1.573-.421-1.834-.919l-.498 1.902c-.181.695-.669 1.566-.995 2.097A8 8 0 1 0 8 0" />
+            </svg>
+          </a>
+          <button class="btn rounded-pill btn-grad text-dark fw-bold m-0"  onclick="window.location.href='#freeQuoteBtn'">Write Review</button>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.btn-color {
+  background-color: #101010;
+}
+         
+         
+.btn-grad {
+            background-image: linear-gradient(to right, #AAFFA9 0%, #11FFBD  51%, #AAFFA9  100%);
+            text-align: center;
+            transition: 0.5s;
+            background-size: 200% auto;
+          }
+
+          .btn-grad:hover {
+            background-position: right center; /* change the direction of the change here */
+            color: #fff;
+            text-decoration: none;
+          }
+         
+         
+
+.glass-button {
+  background-color: rgba(255, 255, 255, 0.1);
+  /* Adjust the opacity (0.3) as needed */
+  backdrop-filter: blur(10px);
+  /* Adjust the blur value as needed */
+}
+
+.btn-border-color {
+  border: 1px solid #CFDD51;
+}
+
+.text-color {
+  color: #cfdd51;
+}
+
+.toggler-icon {
+  width: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  cursor: pointer;
+}
+
+.line {
+  width: 100%;
+  height: 3px;
+  background-color: #cfdd51;
+}
+
+.bg-color {
+  background-color: rgba(255, 255, 255, 0.1);
+  /* Adjust the opacity (0.3) as needed */
+  backdrop-filter: blur(10px);
+}
+
+.secondary-bg-color {
+  background-color: #f3f3f3;
+}
+
+.navbar-toggler {
+  border: none !important;
+  box-shadow: none !important;
+}
+
+.sticky {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 9999;
+}
+</style>
+
+<script>
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+
+export default {
+  setup() {
+    const isMobile = ref(false);
+
+    onMounted(() => {
+      isMobile.value = window.innerWidth <= 480;
+
+      window.addEventListener('resize', updateIsMobile);
+      window.addEventListener('scroll', makeNavBarSticky);
+    });
+
+    onBeforeUnmount(() => {
+      window.removeEventListener('resize', updateIsMobile);
+      window.removeEventListener('scroll', makeNavBarSticky);
+    });
+
+    const updateIsMobile = () => {
+      isMobile.value = window.innerWidth <= 768;
+    };
+
+    const makeNavBarSticky = () => {
+      const navbar = document.getElementById("home");
+      const sticky = 64;
+
+      if (navbar) {
+        if (window.scrollY >= sticky) {
+          navbar.classList.add("position-sticky", "sticky");
+        } else {
+          navbar.classList.remove("position-sticky", "sticky");
+        }
+      } else {
+        console.error('Navbar element not found.');
+      }
+    };
+
+    return {
+      isMobile,
+      makeNavBarSticky,
+      updateIsMobile,
+    };
+  }
+}
+</script>
