91f08720bab96a8285027dc083899a7614affdfb
Yohan Garcia <yoanvaldes03@gmail.com>
2024-08-15 22:38:39 -0400

    testing new design

--- src/components/GenerateReviewSection.vue
+++ src/components/GenerateReviewSection.vue
@@ -1,28 +1,25 @@
 <template>
   <form @submit.prevent="generateWords">
-    <div class="mb-3 pt-3">
-      <label class="form-label">Enter a product or service:</label>
-      <div class="row d-flex justify-content-center">
-        <div class="col-8 p-0">
-        <input class="form-control" type="text" v-model="productOrService">
-      </div>
-      <div class="col-2 p-0">
-        <button class="btn rounded-pill btn-grad text-dark fw-bold m-0">
-        <div v-if="!displaySpinnerWord">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
-            <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"/>
-          </svg>
-        </div>
-        <span v-if="displaySpinnerWord">
-          <div class="spinner-border spinner-border-sm text-dark" role="status">
-            <span class="visually-hidden">Loading...</span>
+    <div class="mb-3 pt-3 m-2">
+      <div class="d-flex align-items-center">
+        <input class="form-control me-2" type="text" v-model="productOrService" placeholder="Enter product or service">
+        <button class="btn rounded-pill btn-grad text-dark fw-bold" type="submit">
+          <div v-if="!displaySpinnerWord">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send"
+              viewBox="0 0 16 16">
+              <path
+                d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z" />
+            </svg>
           </div>
-        </span>
-      </button>
-      </div>
+          <span v-if="displaySpinnerWord">
+            <div class="spinner-border spinner-border-sm text-dark" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+          </span>
+        </button>
       </div>
     </div>
-    <div class="mt-4">
+    <div class="mt-4 m-3">
       <div v-if="positiveWords.length > 0">
         <div class="word-bubbles">
           <span v-for="word in positiveWords" :key="word"
@@ -44,6 +41,11 @@
       v-if="positiveWords.length > 0 || negativeWords.length > 0" @click="generateReview">
       <div v-if="!displaySpinnerReview">
         Generate review
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send"
+          viewBox="0 0 16 16">
+          <path
+            d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z" />
+        </svg>
       </div>
       <span v-if="displaySpinnerReview">
         <div class="spinner-border spinner-border-sm text-dark" role="status">
