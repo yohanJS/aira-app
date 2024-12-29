e57f9ca148321a7a4624762b99828acd4bf3d10a
Yohan Garcia <yoanvaldes03@gmail.com>
2024-12-28 23:02:33 -0500

    dumb

--- src/components/FetchData.vue
+++ src/components/FetchData.vue
@@ -132,7 +132,7 @@
           loading: false,
           services: null,
           serviceId: null,
-          isPrd: false,
+          isPrd: true,
           startOfWeek: moment().startOf('week').format('DD'),
           endOfWeek: moment().endOf('week').format('DD'),
           GetservicesUrl: "",
